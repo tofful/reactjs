22c3ad2aa8920c47ba8df7823158198af01d29aa
Matias Tofful <matias@tofful.com>
2026-07-02 20:22:06 -0300

    Add login form and styles

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,35 @@
-import Navigation from '@/components/blocks/Navigation/Navigation'
-
+import { Link } from '@tanstack/react-router'
 import styles from './Login.module.css'
+import Button from '@/components/ui/Button/Button'
 
 function Login() {
   return (
     <main className={styles.container}>
-      <Navigation />
-      <h1>Hello Login</h1>
+
+      <section className={styles.left}>
+        <div className={styles.form}>
+          <h1 className={styles.title}>Iniciar Sesión</h1>
+          <p className={styles.subtitle}>Ingresá tu email y contraseña para continuar</p>
+
+          <label className={styles.label} htmlFor="email">Email</label>
+          <input className={styles.input} id="email" type="email" placeholder="tu@email.com" />
+
+          <label className={styles.label} htmlFor="password">Contraseña</label>
+          <input className={styles.input} id="password" type="password" placeholder="Contraseña" />
+
+          <Button variant="primary" type="submit">Iniciar Sesión</Button>
+
+          <p className={styles.footer}>
+            ¿No tenés cuenta? <Link to="/register">Registrate</Link>
+          </p>
+        </div>
+      </section>
+
+      <section className={styles.right}>
+
+        LADO DERECHO DE LA PÁGINA
+      </section>
+
     </main>
   )
 }
