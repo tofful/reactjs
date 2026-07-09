314cafa61eac308427ca288926c67cec2ee83f25
Matias Tofful <matias@tofful.com>
2026-07-09 11:18:35 -0300

    Add Register page and Button component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,35 @@
-import Navigation from '@/components/blocks/Navigation/Navigation'
-
+import { Link } from '@tanstack/react-router'
 import styles from './Register.module.css'
+import Button from '@/components/ui/Button/Button'
 
 function Register() {
   return (
     <main className={styles.container}>
-      <Navigation />
-      <h1>Hello Register</h1>
+
+      <section className={styles.left}>
+        <div className={styles.form}>
+          <h1 className={styles.title}>Crear Cuenta</h1>
+          <p className={styles.subtitle}>Completá tus datos para registrarte</p>
+
+          <label className={styles.label} htmlFor="name">Nombre</label>
+          <input className={styles.input} id="name" type="text" placeholder="Tu nombre" />
+
+          <label className={styles.label} htmlFor="email">Email</label>
+          <input className={styles.input} id="email" type="email" placeholder="tu@email.com" />
+
+          <label className={styles.label} htmlFor="password">Contraseña</label>
+          <input className={styles.input} id="password" type="password" placeholder="••••••••" />
+
+          <Button variant="primary" type="submit">Registrarse</Button>
+
+          <p className={styles.footer}>
+            ¿Ya tenés cuenta? <Link to="/login">Iniciá sesión</Link>
+          </p>
+        </div>
+      </section>
+
+      <section className={styles.right}></section>
+
     </main>
   )
 }
